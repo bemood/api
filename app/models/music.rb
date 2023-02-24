5e3c910e20f4e12b8d783bd3edc1c1526b0ad970
YanisDahmane <113184746+YanisDahmane@users.noreply.github.com>
2023-02-24 19:03:44 +0100

    amen

--- app/models/music.rb
+++ app/models/music.rb
@@ -2,22 +2,19 @@ class Music < ApplicationRecord
   has_many :posts
 
   def render
+    track = RSpotify::Track.find(self.spotify_id)
+
     return {
       id: self.id,
       spotify_id: self.spotify_id,
-      music: spotify_music
-    }
-  end
-
-  def spotify_music
-    track = RSpotify::Track.find(self.spotify_id.to_s)
-    {
-      id: track.id,
-      name: track.name,
-      artist: track.artists[0].name,
-      album: track.album.name,
-      image: track.album.images[0]['url'],
-      preview_url: track.preview_url
+      music: {
+        id: track.id,
+        name: track.name,
+        artist: track.artists[0].name,
+        album: track.album.name,
+        image: track.album.images[0]['url'],
+        preview_url: track.preview_url
+      }
     }
   end
 end
