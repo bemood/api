3bbe9c40b881fccc6bcebd9b8519ebd11393588e
noahGms <noah@keshin.net>
2023-04-12 02:01:37 +0200

    use env variable in rspotify initiliazer instead clear key
    + install dotenv-rails package to load automatically vairable

--- config/initializers/rspotify.rb
+++ config/initializers/rspotify.rb
@@ -1,1 +1,4 @@
-RSpotify::authenticate("06bb38c2b8d24c9b83bebffbcd37d8f6", "d3a999be761a412e928c09c117183405")
+RSpotify::authenticate(
+  ENV['SPOTIFY_CLIENT_ID'],
+  ENV['SPOTIFY_CLIENT_SECRET']
+)
